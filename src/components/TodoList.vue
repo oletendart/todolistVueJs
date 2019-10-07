7e25280429000bcf6acb39439844a342742137ba
Olivia Letendart <letendartolivia@gmail.com>
2019-10-07 11:52:33 +0200

    Init repo

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <TodoInputText />
+    </div>
+</template>
+
+<script>
+import TodoInputText from "@/components/TodoInputText.vue";
+
+export default {
+    components: {
+        TodoInputText,
+    }
+};
+</script>
+
+<style>
+
+</style>
