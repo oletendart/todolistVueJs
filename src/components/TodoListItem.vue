c0d7354c2d2468d444d96459346def7796251ff8
Olivia Letendart <letendartolivia@gmail.com>
2019-10-07 16:03:50 +0200

    Add TodoListItem.vue

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+     <ul>
+         <li v-for="todo in todos">{{ todo }}</li>
+     </ul>
+  </div>
+</template>
+
+<script>
+export default {
+   props: ["todos"]
+}
+</script>
+
+<style>
+
+</style>
